0fb32aa2ef76bb34a18ac2f169d1c4637830e9ae
Valentin Fortun <Valentin@MacBook-Pro-de-Valentin.local>
2016-03-08 16:07:05 +0100

    Ajoût du bouton pour ajouter les sports pratiqués

--- InscriptionTermine.php
+++ InscriptionTermine.php
@@ -6,8 +6,8 @@
     </head>
     <body>
     	<p>
-        <a href="AccueilVisiteurs.php"><img src="Logo.png" 	width="306" height="172" alt=""></a>  
-	 
+        <a href="AccueilVisiteurs.php"><img src="Logo.png" 	width="306" height="172" alt=""></a>
+
 <p> Bienvenue  <?php $Prenom=$_POST['Prenom'] ;
 echo $Prenom; ?> !</p>
 <p> Votre adresse mail : <?php $Email=$_POST['Email'] ;
@@ -16,5 +16,6 @@ echo $Email; ?>
 	<p> Terminez de complèter votre profil :</p>
     <p> Ajouter une photo de Profil : <input type="file" name="Photo"/>
     <a href="" > <input type="button" name="Upload" value="Upload"/> </a> </p>
+    <p> Ajoutez les <a href="" > <input type="button" name="Sports" value="Sports"/> </a> que vous pratiquez </p>
     </body>
 </html>
