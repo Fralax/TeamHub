8daececb2fe67a2f35022300e9d5f06732d58f20
Fralax <frayssinetr@gmail.com>
2016-03-10 20:20:52 +0100

    Pull Valentin 2

--- Modèles/modele.php
+++ Modèles/modele.php
@@ -1,7 +1,7 @@
 <?php
 
 function getBdd() {
-  $bdd = new PDO('mysql:host=localhost; dbname=TeamHub;charset=utf8', 'root', 'root');
+  $bdd = new PDO('mysql:host=localhost; dbname=TeamHub; charset=utf8', 'root', 'root');
   return $bdd;
 }
 
