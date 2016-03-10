28f5e8c36dcc4c8367d133f26ae04de564e49adf
Val78 <valentin.fortun@orange.fr>
2016-03-10 20:15:57 +0100

    inscription

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require 'Modèles/modele.php';
+try {
+  $utilisateurs = getUtilisateurs();
+  require 'Vues/vueFormulaire.php';
+}
+catch (Exception $e) {
+  $msgErreur = $e->getMessage();
+  require 'Vues/vueErreur.php';
+}
+?>
