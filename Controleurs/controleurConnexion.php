9e1081c3fcc2f8a6060ab59ded713a72c25cf83e
Val78 <valentin.fortun@orange.fr>
2016-03-12 22:24:08 +0100

    inscription + connexion

--- Controleurs/controleurConnexion.php
+++ Controleurs/controleurConnexion.php
@@ -2,6 +2,22 @@
 
 require 'Modeles/modeleConnexion.php';
 
+function connexionUtilisateurs(){
+  $connexion = $_POST['connexion'];
+  if (isset($connexion) && $connexion == 'Connexion'){
+    $resultat = verifID();
+    if (!$resultat) {
+        echo 'Mauvais identifiant ou mot de passe !';
+    }
+    else {
+        session_start();
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['pseudo'] = $_POST['pseudo'];
+        echo 'Vous êtes connecté !';
+    }
+  }
+}
+
 try {
   require 'Vues/vueAccueilVisiteurs.php';
   connexionUtilisateurs();
