4c1a7fa40657452828aaa26b3d1729ca929bfbd1
Fralax <frayssinetr@gmail.com>
2016-03-29 18:52:41 +0200

    Css

--- Vues/gabaritMembres.php
+++ Vues/gabaritMembres.php
@@ -1,41 +1,40 @@
 <!doctype html>
 <html lang="fr">
+
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="Contenu/style.css" />
         <title><?= $titre ?></title>
     </head>
+
     <body>
-        <div id="global">
 
-            <header>
-              <div id="gauche"> <a href="index.php"><img id="logo" src="/TeamHub/Autres/Logo.png" width="306" height="172" ></a> </div>
-              <div id="droite">
-              <?php echo "Bonjour, ", $_POST['pseudo'], " ! "; ?>
-
-              <ul>
-				        <li><a href="index.php?page=accueilmembres"> Accueil </a></li>
-				        <li><a href="index.php?page="> Groupe </a></li>
-				        <li><a href="index.php?page="> Club </a></li>
-				        <li><a href="index.php?page="> Profil </a></li>
-			       </ul>
-
-           </div>
-
-            </header>
-
-            <div id="contenu">
-                <?= $contenu ?>
-            </div>
-
-            <footer id="piedBlog">
-              <div>
-                <a href="index.php?page=aproposmembres"> A propos </a>
-              </div>
-
-              Site réalisé par Valentin Fortun et Romain Frayssinet
-            </footer>
-
+      <header>
+        <div id="headerGauche"> <a href="index.php"><img id="logo" src="/TeamHub/Autres/Logo.png" width="306" height="172" ></a> </div>
+        <div id="headerDroite">
+        <?php echo "Bonjour, ", $_POST['pseudo'], " ! "; ?>
+        <ul>
+          <li><a href="index.php?page=accueilmembres"> Accueil </a></li>
+          <li><a href="index.php?page="> Groupe </a></li>
+          <li><a href="index.php?page="> Club </a></li>
+          <li><a href="index.php?page="> Profil </a></li>
+        </ul>
         </div>
+      </header>
+
+      <div id="wrap">
+        <div id="main" class="clearfix">
+          <?= $contenu ?>
+        </div>
+      </div>
+
+      <footer>
+        <div>
+          <a href="index.php?page=aproposmembres"> A propos </a>
+        </div>
+        Site réalisé par le groupe G5C
+      </footer>
+
     </body>
+
 </html>
--- Vues/gabaritVisiteurs.php
+++ Vues/gabaritVisiteurs.php
@@ -1,43 +1,40 @@
 <!doctype html>
 <html lang="fr">
+
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="Contenu/style.css" />
         <title><?= $titre ?></title>
     </head>
+
     <body>
 
-        <div id="global">
-            <header>
-
-                  <div id= "gauche">
-                    <a href="index.php"><img id="logo" src="/TeamHub/Autres/Logo.png" width="306" height="172" ></a>
-                  </div>
-
-                  <div id= "droite">
-              		  <form action="" id="formulaireAccueil" name="formulaireAccueil" method="post">
-              			  <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required>
-                		  <input type="password" name="PasswordAccueil" id="passwordaccueil" placeholder="Mot de Passe" required>
-                		  <input name="connexion" type="submit" id="connexion" value = "Connexion">
-              		  </form>
-            			  <a href="index.php?page=inscription"> <input name="inscription" type="button" id="inscription" value = "Inscription"> </a>
-                  </div>
-            		</p>
-
-            </header>
-
-            <div id="contenu">
-                <?= $contenu ?>
-            </div>
-
-            <footer>
-              <div id="piedBlog">
-                <a href="index.php?page=aproposvisiteurs"> A propos </a>
-
-                Site réalisé par Valentin Fortun et Romain Frayssinet
-              </div>
-            </footer>
-
+      <header>
+        <div id= "headerGauche">
+          <a href="index.php"><img id="logo" src="/TeamHub/Autres/Logo.png" width="306" height="172" ></a>
         </div>
+        <div id= "headerDroite">
+          <form action="" id="formulaireAccueil" name="formulaireAccueil" method="post">
+            <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required>
+            <input type="password" name="PasswordAccueil" id="passwordaccueil" placeholder="Mot de Passe" required>
+            <input name="connexion" type="submit" id="connexion" value = "Connexion">
+            <a href="index.php?page=inscription"> <input name="inscription" type="button" id="inscription" value = "Inscription"> </a>
+          </form>
+        </div>
+      </header>
+
+      <div id="wrap">
+        <div id="main" class="clearfix">
+          <?= $contenu ?>
+        </div>
+      </div>
+
+      <footer>
+        <div>
+          <a href="index.php?page=aproposmembres"> A propos </a>
+        </div>
+        Site réalisé par le groupe G5C
+      </footer>
+
     </body>
 </html>
