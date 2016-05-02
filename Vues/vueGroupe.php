5c457067dbf673dcede3bdce3efa735b5576103a
Fralax <frayssinetr@gmail.com>
2016-05-03 00:13:14 +0200

    Test

--- Vues/vueGroupe.php
+++ Vues/vueGroupe.php
@@ -31,59 +31,62 @@
 <?php }
     else{     //sinon
       if(isset($_SESSION['pseudo'])){
+        $i = 0;
         foreach ($membres as list($membre)) {
-          if ($membre == $_SESSION['pseudo']){ ?>
-
-            <p> Description du groupe :
-            <?php echo $caract['g_description'] ?>
-            </p>
-            <p> Administrateur :
-            <?php echo $caract['g_admin'] ?>
-            </p>
-            <p> Nombre de places :
-            <?php echo $caract['g_placesLibres'] ?>
-            </p>
-            <p> Sport : <?php echo $caract['g_sport'] ?></p>
-            <p> Lieu : <?php echo $caract['g_departement'] ?></p>
-            <p> <a href = "index.php?page=creationevenements&nom=<?php echo $caract['g_nom']?>" > <input type = "button" name="Evenement" value="Créer un événement" > </a> </p>
-      <?php break;
+          if ($membre == $_SESSION['pseudo']){
+            $i = 1;
           }
-
           if ($membre != $_SESSION['pseudo']){
-            if($caract['g_placesLibres'] !=0){ ?>
-
-              <p> Description du groupe :
-              <?php echo $caract['g_description'] ?>
-              </p>
-              <p> Administrateur :
-              <?php echo $caract['g_admin'] ?>
-              </p>
-              <p> Nombre de places :
-              <?php echo $caract['g_placesLibres'] ?>
-              </p>
-              <p> Sport : <?php echo $caract['g_sport'] ?></p>
-              <p> Lieu : <?php echo $caract['g_departement'] ?></p>
-              <p> <input type = "button" name="rejoindre" value="Rejoindre ce groupe" > </p>
-      <?php }
-
-            else{ ?>
-
-              <p> Description du groupe :
-              <?php echo $caract['g_description'] ?>
-              </p>
-              <p> Administrateur :
-              <?php echo $caract['g_admin'] ?>
-              </p>
-              <p> Nombre de places :
-              <?php echo $caract['g_placesLibres'] ?>
-              </p>
-              <p> Sport : <?php echo $caract['g_sport'] ?></p>
-              <p> Lieu : <?php echo $caract['g_departement'] ?></p>
-
-      <?php }
-            break;
+            if($caract['g_placesLibres'] !=0){
+              $i=2;
+            } else{
+              $i=3;
+            }
           }
         }
+
+        if ($i = 1){ ?>
+          <p> Description du groupe :
+          <?php echo $caract['g_description'] ?>
+          </p>
+          <p> Administrateur :
+          <?php echo $caract['g_admin'] ?>
+          </p>
+          <p> Nombre de places :
+          <?php echo $caract['g_placesLibres'] ?>
+          </p>
+          <p> Sport : <?php echo $caract['g_sport'] ?></p>
+          <p> Lieu : <?php echo $caract['g_departement'] ?></p>
+          <p> <a href = "index.php?page=creationevenements&nom=<?php echo $caract['g_nom']?>" > <input type = "button" name="Evenement" value="Créer un événement" > </a> </p>
+
+  <?php }
+        if($i = 2){ ?>
+          <p> Description du groupe :
+          <?php echo $caract['g_description'] ?>
+          </p>
+          <p> Administrateur :
+          <?php echo $caract['g_admin'] ?>
+          </p>
+          <p> Nombre de places :
+          <?php echo $caract['g_placesLibres'] ?>
+          </p>
+          <p> Sport : <?php echo $caract['g_sport'] ?></p>
+          <p> Lieu : <?php echo $caract['g_departement'] ?></p>
+          <p> <input type = "button" name="rejoindre" value="Rejoindre ce groupe" > </p>
+  <?php }
+        if ($i = 3){ ?>
+          <p> Description du groupe :
+          <?php echo $caract['g_description'] ?>
+          </p>
+          <p> Administrateur :
+          <?php echo $caract['g_admin'] ?>
+          </p>
+          <p> Nombre de places :
+          <?php echo $caract['g_placesLibres'] ?>
+          </p>
+          <p> Sport : <?php echo $caract['g_sport'] ?></p>
+          <p> Lieu : <?php echo $caract['g_departement'] ?></p>
+  <?php }
       }
       else{ ?>
 
