46f4bef8fc3a0bbc81858c7876908c72f2425e56
Val78 <valentin.fortun@orange.fr>
2016-05-03 09:23:11 +0200

    merge master

--- Controleurs/controleurConnexion.php
+++ Controleurs/controleurConnexion.php
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-require 'Modeles/modeleConnexion.php';
-
-function connexionUtilisateurs(){
-<<<<<<< HEAD
-  $connexion = $_POST['connexion'];
-  if (isset($connexion) && $connexion == 'Connexion'){
-    $resultat = verifID();
-    if (!$resultat) {
-        echo 'Mauvais identifiant ou mot de passe !';
-    }
-=======
-
-  $resultat = verifID();
-  $connexion = $_POST['connexion'];
-
-  if (isset($connexion) && $connexion == 'Connexion'){
-    if (!$resultat) {
-        echo 'Mauvais identifiant ou mot de passe !';
-    }
-
->>>>>>> f48ed8bd5311df03e5c448007350505fca0713c8
-    else {
-        session_start();
-        $_SESSION['id'] = $resultat['id'];
-        $_SESSION['pseudo'] = $_POST['pseudo'];
-        echo 'Vous êtes connecté !';
-
-    }
-  }
-}
-<<<<<<< HEAD
-=======
-
->>>>>>> f48ed8bd5311df03e5c448007350505fca0713c8
-
-try {
-  require 'Vues/vueAccueilVisiteurs.php';
-  connexionUtilisateurs();
-}
-
-catch (Exception $e) {
-  $msgErreur = $e->getMessage();
-  require 'Vues/vueErreur.php';
-}
-?>
-=======
 <?php
 
 require_once 'Modeles/utilisateurs.php';
@@ -81,4 +32,3 @@ class connexion{
 }
 
 ?>
->>>>>>> 33c2dcf6fa3c61c857735f20171a690ed1910e92
