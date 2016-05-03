614de37ad80567cf3c9c3f865b054febc403f2db
Val78 <valentin.fortun@orange.fr>
2016-05-03 11:11:54 +0200

    suite événements

--- Modeles/evenements.php
+++ Modeles/evenements.php
@@ -7,10 +7,17 @@ class evenements extends modele {
   public function ajouterEvenementsBdd($groupe){
     $date = "{$_POST['annee']}-{$_POST['mois']}-{$_POST['jour']}";
     $heure = "{$_POST['heure']}:{$_POST['minute']}:00";
-    $sql = 'INSERT INTO Evenements(e_date, e_heure, e_createur, g_nom, c_nom)
-            VALUES (:dateActivite, :heureActivite, :createur ,:nomGroupe, :nomClub)';
-    $ajouterEvenementsBdd = $this->executerRequete ($sql, array('dateActivite'=>$date , 'heureActivite'=> $heure, 'createur'=> $_SESSION['pseudo'], 'nomGroupe'=> $groupe, 'nomClub'=> $_POST['club']));
+    $sql = 'INSERT INTO Evenements(e_nom, e_date, e_heure, e_createur, g_nom, c_nom)
+            VALUES (:nomEvenement ,:dateActivite, :heureActivite, :createur ,:nomGroupe, :nomClub)';
+    $ajouterEvenementsBdd = $this->executerRequete ($sql, array('nomEvenement'=>$_POST['nomEvenement'] ,'dateActivite'=>$date , 'heureActivite'=> $heure, 'createur'=> $_SESSION['pseudo'], 'nomGroupe'=> $groupe, 'nomClub'=> $_POST['club']));
   }
+
+  public function afficherEvenements($groupe){
+    $sql = 'SELECT e_nom, e_date, e_heure, e_createur, c_nom  FROM Evenements WHERE g_nom = ?';
+    $afficherEvenements = $this->executerRequete ($sql, array($groupe));
+    return $afficherEvenements;
+  }
+
 
 }
 ?>
