d188b7616960f423bbfdada046de434f965ba9a2
Val78 <valentin.fortun@orange.fr>
2016-05-08 22:37:28 +0200

    modif club non fonctionnel

--- Modeles/club.php
+++ Modeles/club.php
@@ -5,12 +5,35 @@ require_once "Modeles/modele.php";
 class club extends modele {
 
   public function ajouterClubBdd(){
-    $sql = 'INSERT INTO Clubs(c_nom, c_adresse, c_cp) VALUES (:nomClub, :adresseClub, :cpClub)';
-    $ajouterClubBdd = $this->executerRequete ($sql, array('nomClub'=>$_POST['nomClub'], 'adresseClub'=>$_POST['adresseClub'], 'cpClub'=>$_POST['cpClub']));
+
+    $hoLundiDebut = "{$_POST['hLundiDebut']}:{$_POST['mLundiDebut']}:00";
+    $hoMardiDebut = "{$_POST['hMardiDebut']}:{$_POST['mMardiDebut']}:00";
+    $hoMercrediDebut = "{$_POST['hMercrediDebut']}:{$_POST['mMercrediDebut']}:00";
+    $hoJeudiDebut = "{$_POST['hJeudiDebut']}:{$_POST['mJeudiDebut']}:00";
+    $hoVendrediDebut = "{$_POST['hVendrediDebut']}:{$_POST['mVendrediDebut']}:00";
+    $hoSamediDebut = "{$_POST['hSamediDebut']}:{$_POST['mSamediDebut']}:00";
+    $hoDimancheDebut = "{$_POST['hDimancheDebut']}:{$_POST['mDimancheDebut']}:00";
+    $hoLundiFin = "{$_POST['hLundiFin']}:{$_POST['mLundiFin']}:00";
+    $hoMardiFin = "{$_POST['hMardiFin']}:{$_POST['mMardiFin']}:00";
+    $hoMercrediFin = "{$_POST['hMercrediFin']}:{$_POST['mMercrediFin']}:00";
+    $hoJeudiFin = "{$_POST['hJeudiFin']}:{$_POST['mJeudiFin']}:00";
+    $hoVendrediFin = "{$_POST['hVendrediFin']}:{$_POST['mVendrediFin']}:00";
+    $hoSamediFin = "{$_POST['hSamediFin']}:{$_POST['mSamediFin']}:00";
+    $hoDimancheFin = "{$_POST['hDimancheFin']}:{$_POST['mDimancheFin']}:00";
+
+    $sql = 'INSERT INTO Clubs(c_nom, c_adresse, c_cp, c_numero, c_hoLundiDebut, c_hoMardiDebut, c_hoMercrediDebut, c_hoJeudiDebut, c_hoVendrediDebut, c_hoSamediDebut,
+                              c_hoDimancheDebut, c_hoLundiFin, c_hoMardiFin, c_hoMercrediFin, c_hoJeudiFin, c_hoVendrediFin, c_hoSamediFin, c_hoDimancheFin, c_hoCommentaire)
+            VALUES (:nomClub, :adresseClub, :cpClub, :numeroClub, :hoLundiDebutClub, :hoMardiDebutClub, :hoMercrediDebutClub, :hoJeudiDebutClub, :hoVendrediDebutClub, :hoSamediDebutClub, :hoDimancheDebutClub
+                    :hoLundiFinClub, :hoMardiFinClub, :hoMercrediFinClub, :hoJeudiFinClub, :hoVendrediFinClub, :hoSamediFinClub, :hoDimancheFinClub, :hoCommentaireClub)';
+    $ajouterClubBdd = $this->executerRequete ($sql, array('nomClub'=>$_POST['nomClub'], 'adresseClub'=>$_POST['adresseClub'], 'cpClub'=>$_POST['cpClub'], 'numeroClub'=>$_POST['numeroClub'], 'hoLundiDebutClub'=>$hoLundiDebut, 'hoMardiDebutClub'=>$hoMardiDebut, 'hoMercrediDebutClub'=>$hoMercrediDebut,
+     'hoJeudiDebutClub'=>$hoJeudiDebut, 'hoVendrediDebutClub'=>$hoVendrediDebut, 'hoSamediDebutClub'=>$hoSamediDebut, 'hoDimancheDebutClub'=>$hoDimancheDebut, 'hoLundiFinClub'=>$hoLundiFin, 'hoMardiFinClub'=>$hoMardiFin, 'hoMercrediFinClub'=>$hoMercrediFin, 'hoJeudiFinClub'=>$hoJeudiFin,
+     'hoVendrediFinClub'=>$hoVendrediFin, 'hoSamediFinClub'=>$hoSamediFin, 'hoDimancheFinClub'=>$hoDimancheFin, 'hoCommentaireClub'=>$_POST['remarqueHoraire']));
   }
 
   public function listerClub(){
-    $sql = 'SELECT c_nom, c_adresse, c_cp FROM Clubs';
+    $sql = 'SELECT c_nom, c_adresse, c_cp, c_numero, c_hoLundiDebut, c_hoMardiDebut, c_hoMercrediDebut, c_hoJeudiDebut, c_hoVendrediDebut, c_hoSamediDebut,
+                   c_hoDimancheDebut, c_hoLundiFin, c_hoMardiFin, c_hoMercrediFin, c_hoJeudiFin, c_hoVendrediFin, c_hoSamediFin, c_hoDimancheFin, c_hoCommentaire
+            FROM Clubs';
     $listerClub = $this->executerRequete ($sql);
     return $listerClub;
   }
--- Vues/vueAjoutClub.php
+++ Vues/vueAjoutClub.php
@@ -12,6 +12,195 @@
 				<p> Nom du club : <input type="text" name="nomClub" placeholder="Nom du Club" size="25" value = "<?= $_POST['nomClub'] ?>"/> </p>
         <p> Adresse : <input type="text" name="adresseClub" placeholder="Adresse du Club" size="25" value = "<?= $_POST['adresseClub'] ?>"/> </p>
         <p> CP : <input type="text" name="cpClub" placeholder="Code Postal" size="25" value = "<?= $_POST['cpClub'] ?>"/> </p>
+				<p> Numéro de téléphone : <input type="text" name="numeroClub" placeholder="Téléphone" size="25" value = "<?= $_POST['numeroClub'] ?>"/>
+				<p> Horaires :</p>
+				<p>	Le Lundi  De
+		        <select name="hLundiDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mLundiDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hLundiFinClub">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mLundiFinClub">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Mardi  De
+		        <select name="hMardiDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mMardiDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hMardiFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mMardiFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Mercredi  De
+		        <select name="hMercrediDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mMercrediDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hMercrediFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mMercrediFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Jeudi  De
+		        <select name="hJeudiDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mJeudiDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hJeudiFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mJeudiFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Vendredi  De
+		        <select name="hVendrediDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mVendrediDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hVendrediFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mVendrediFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Samedi  De
+		        <select name="hSamediDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mSamediDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hSamediFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mSamediFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+				<p>	Le Dimanche  De
+		        <select name="hDimancheDebut">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select> à
+		        <select name="mDimancheDebut">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+						à <select name="hDimancheFin">
+		          <option value=""> Heure </option>
+		          <?php for ($heure = 00 ; $heure <= 23 ; $heure++){ ?>
+		          <option value = "<?php echo $heure ?>" <?php if ($_POST['heure']=="$heure"){?> selected <?php }?> > <?php echo $heure; ?> </option>
+		          <?php } ?>
+		        </select>
+		        <select name="mDimancheFin">
+		          <option value=""> Minute </option>
+		          <?php for ($minute = 00 ; $minute <= 59 ; $minute=$minute+10){ ?>
+		          <option value= "<?php echo $minute ?>" <?php if ($_POST['minute']=="$minute"){?> selected <?php }?> > <?php echo $minute; ?> </option>
+		          <?php } ?>
+		        </select>
+				</p>
+
+				<p>	<label for="remarqueHoraire"> Ajoutez une remarque sur les horaires si vous le souhaitez </label><br />
+	        <textarea name="remarqueHoraire" > </textarea> </p>
+				<!-- <p> Lien : <input type="text" name="lienClub" placeholder="Lien vers le site du club" size="40" value = ""/> </p> -->
+				<p> <input type="file" name="photo" /> </p>
 
 				<p> <input name="ajouter" type="submit" value="Ajouter"> </p>
 			</form>
