6efde8791d6f66cc33e81f8d7754cfeca5b1bb54
Val78 <valentin.fortun@orange.fr>
2016-05-15 23:03:26 +0200

    fix

---
+++ Vues/vueBanni.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Vous êtes Banni ! </title>
+  </head>
+
+  <body>
+
+    <h2> Vous avez êtes Banni ! </h2>
+
+    <p> Si vous souhaitez plus de détail, contactez l'administrateur du site. </p>
+
+  </body>
+</html>
